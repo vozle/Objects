c5faa01af614d40eeb24fee4937afc9d2eedd4d9
vozle <vozle2@gmail.com>
2022-08-24 18:42:03 +0300

    Initial commit

---
+++ task.js
@@ -0,0 +1,39 @@
+function Student(name, gender, age) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+}
+
+Student.prototype.setSubject = function (subjectName) {
+  this.subject = subjectName;
+}
+
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...mark) {
+  if (this.marks === undefined) {
+    this.marks = [...mark];
+  } else {
+    this.marks.push(...mark);
+  }
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  for (let mark of this.marks) {
+    sum += mark;
+  }
+  return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
